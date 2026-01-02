e8e2f9c6b8b23e8ed1694e8d2f9151fb01add276
Alexander Tregubov (aka (@)AT) <tregubovav.dev@gmail.com>
2026-01-01 21:12:55 -0800

    Added: New demo
    Signed-off-by: Alexander Tregubov (aka (@)AT) <tregubovav.dev@gmail.com>

---
+++ Source/Records/04 - Atomic Set/Records_04_AtomicSet.dpr
@@ -0,0 +1,178 @@
+program Records_05_AtomicSet;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System.SysUtils,
+  System.Classes,
+  System.SyncObjs,
+  System.Generics.Collections,
+  Playground.Utils in '..\..\Common\Playground.Utils.pas',
+  Playground.Records.AtomicSet in 'Playground.Records.AtomicSet.pas';
+
+// -----------------------------------------------------------------------------
+// Example 1: Syntax & Operators
+// -----------------------------------------------------------------------------
+procedure Example1;
+var
+  lSet: TAtomicSet;
+begin
+  Writeln('--- Example #1: Syntax & Operators ---');
+  Writeln('Objective: Demonstrate Pascal-like syntax on the atomic wrapper.');
+
+  // 1. Implicit Assignment
+  lSet := [afRunning, afCanceling];
+  Writeln(sLineBreak + '  [Code] lSet := [afRunning, afCanceling];');
+
+  // Use AsInteger for Hex Output
+  Writeln(Format('  [Result] As Hex: $%x', [lSet.AsInteger]));
+
+  // 2. Set Arithmetic (+)
+  Writeln(sLineBreak + '  [Code] lSet := lSet + afFailing;');
+  lSet := lSet + afFailing;
+
+  // 3. "In" Operator with clean ToString
+  Writeln('  [Result] Is afFailed in Set? ' +
+    (afFailing in lSet).ToString(TUseBoolStrs.True));
+
+  // 4. Subtract (-)
+  Writeln(sLineBreak + '  [Code] lSet := lSet - [afRunning];');
+  lSet := lSet - [afRunning];
+
+  Writeln('  [Result] Is afRunning in Set? ' +
+    (afRunning in lSet).ToString(TUseBoolStrs.True));
+end;
+
+// -----------------------------------------------------------------------------
+// Example 2: Atomic API
+// -----------------------------------------------------------------------------
+procedure Example2;
+var
+  lSet: TAtomicSet;
+begin
+  Writeln(sLineBreak + '--- Example #2: Atomic Operations ---');
+  Writeln('Objective: Thread-safe modifications.');
+
+  lSet := [];
+
+  // 1. Atomic Include
+  Writeln(sLineBreak + '  [Code] AtomicInclude(afRunning)');
+  lSet.AtomicInclude(afRunning);
+
+  if afRunning in lSet then
+    Writeln('  [Result] afRunning set. Value: $' +
+      lSet.AsInteger.ToHexString(4));
+
+  // 2. Atomic Transition (CAS)
+  Writeln(sLineBreak +
+    '  [Code] AtomicTransition([afRunning], [afRunning, afCanceling])');
+  // Logic: If set contains ONLY afRunning, add afCanceling.
+  if lSet.AtomicTransition([afRunning], [afRunning, afCanceling]) then
+    Writeln('  [Result] Success. Transitioned.')
+  else
+    Writeln('  [Result] Failed.');
+
+  if afCanceling in lSet then Writeln('  [Result] afCanceling is now set.');
+end;
+
+// -----------------------------------------------------------------------------
+// Example 3: The Flag Race (Stress Test)
+// -----------------------------------------------------------------------------
+type
+  PAtomicSet = ^TAtomicSet;
+
+  TFlagSetter = class(TThread)
+  private
+    FTarget: PAtomicSet;
+    FFlag: TAtomicFlag;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(ATarget: PAtomicSet; AFlag: TAtomicFlag);
+  end;
+
+constructor TFlagSetter.Create(ATarget: PAtomicSet; AFlag: TAtomicFlag);
+begin
+  inherited Create(True);
+  FTarget := ATarget;
+  FFlag := AFlag;
+  FreeOnTerminate := False;
+end;
+
+procedure TFlagSetter.Execute;
+begin
+  // Race condition generator:
+  // Multiple threads try to update the SAME bytes simultaneously.
+  // Standard "OR" would lose bits. AtomicInclude guarantees safety via CAS loop.
+  FTarget^.AtomicInclude(FFlag);
+end;
+
+procedure Example3;
+var
+  lSharedSet: TAtomicSet;
+  lThreads: TObjectList<TFlagSetter>;
+  lEnum: TAtomicFlag;
+  lMissing: Boolean;
+begin
+  Writeln(sLineBreak + '--- Example #3: Multithreaded Flag Race ---');
+  Writeln(
+    Format('Objective: %d Threads set %0:d different flags simultaneously.',
+      [Succ(Ord(High(TAtomicFlag)))])
+  );
+
+  lSharedSet := [];
+  lThreads := TObjectList<TFlagSetter>.Create;
+
+  try
+    // Create 7 threads, each assigned one unique flag (afQueued..afPausing)
+    for lEnum := Low(TAtomicFlag) to High(TAtomicFlag) do
+    begin
+      lThreads.Add(TFlagSetter.Create(@lSharedSet, lEnum));
+    end;
+
+    Writeln(Format('  [Setup] Created %d threads.', [lThreads.Count]));
+
+    // Start all
+    for var t in lThreads do t.Start;
+
+    // Wait for completion
+    for var t in lThreads do t.WaitFor;
+
+    // Verify
+    Writeln(sLineBreak + '  [Verification]');
+    lMissing := False;
+
+    for lEnum := Low(TAtomicFlag) to High(TAtomicFlag) do
+    begin
+      if not (lEnum in lSharedSet) then
+      begin
+        Writeln(Format('  [Error] Flag %d is MISSING!', [Ord(lEnum)]));
+        lMissing := True;
+      end;
+    end;
+
+    if not lMissing then
+      Writeln(Format('  [Success] All flags present. Result: $%x', [lSharedSet.AsInteger]))
+    else
+      Writeln('  [Fail] Race condition detected.');
+
+  finally
+    lThreads.Free;
+  end;
+end;
+
+begin
+  try
+    Example1;
+    NextSlide;
+
+    Example2;
+    NextSlide;
+
+    Example3;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+  PromptForCompletion;
+end.
